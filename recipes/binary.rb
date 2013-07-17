e762f219b57d90ca22b777bc33ba974b6606bf50
OKINAKA Kenshin <okinakak@yahoo.co.jp>
2013-07-17 16:56:44 +0900

    Add recipe for binary.

---
+++ recipes/binary.rb
@@ -0,0 +1,20 @@
+src_filename = "phantomjs-1.9.1-linux-x86_64"
+src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}.tar.bz2"
+extract_path = "#{Chef::Config['file_cache_path']}/#{src_filename}"
+
+package "bzip2"
+
+remote_file src_filepath do
+  source "https://phantomjs.googlecode.com/files/#{src_filename}.tar.bz2"
+  action :create_if_missing
+end
+
+bash 'extract' do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    tar xjf #{src_filepath}
+    cp #{extract_path}/bin/phantomjs /usr/local/bin/
+    cp #{extract_path}/examples/rasterize.js /tmp/
+  EOH
+  not_if { ::File.exists?(extract_path) }
+end
