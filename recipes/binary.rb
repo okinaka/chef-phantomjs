37c7b48a537da30005cb8ab21417e9719f12e331
OKINAKA Kenshin <okinakak@yahoo.co.jp>
2013-08-27 13:53:27 +0900

    recipes/binary.rb: Fix installed path.

--- recipes/binary.rb
+++ recipes/binary.rb
@@ -1,6 +1,7 @@
 src_filename = "phantomjs-1.9.1-linux-x86_64"
 src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}.tar.bz2"
 extract_path = "#{Chef::Config['file_cache_path']}/#{src_filename}"
+install_path = "/usr/local/bin"
 
 package "bzip2"
 
@@ -13,7 +14,7 @@ bash 'extract' do
   cwd ::File.dirname(src_filepath)
   code <<-EOH
     tar xjf #{src_filepath}
-    cp #{extract_path}/bin/phantomjs /usr/local/bin/
+    cp #{extract_path}/bin/phantomjs #{install_path}
     cp #{extract_path}/examples/rasterize.js /tmp/
   EOH
   not_if { ::File.exists?(extract_path) }
